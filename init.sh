aa264bfce4dc1564adb34b8d8a992997061ba8c5
Node <nodar.chkuaselidze@gmail.com>
2016-12-29 01:36:12 +0400

    Export WABT Path

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@
 # Configure Build Tools
 LLVM=$(realpath ../build/llvm/build/bin)
 BINARYEN=$(realpath ../build/binaryen/build/bin)
+WABT=$(realpath ../wabt/out)
 
-export PATH="$PATH:$LLVM:$BINARYEN"
+export PATH="$PATH:$LLVM:$BINARYEN:$WABT"
