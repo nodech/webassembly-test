d261cd7cddaa033c8a9c409959cee6a81a88c699
Node <nodar.chkuaselidze@gmail.com>
2016-12-29 18:25:31 +0400

    Implement mathAverage in C

--- src/test.c
+++ src/test.c
@@ -1,4 +1,14 @@
 
-extern char *test() {
+char *test() {
   return "Hello World";
+}
+
+float mathAverage(int *array, int size) {
+  float sum = 0;
+
+  for (int i = 0; i < size; i++) {
+    sum += array[i];
+  }
+
+  return sum / (float)size;
 }
