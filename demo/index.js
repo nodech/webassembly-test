dd7fdd90b25940da4924f63cc8d9773dcf0b410c
Node <nodar.chkuaselidze@gmail.com>
2016-12-29 13:08:49 +0400

    Add memory?

--- demo/index.js
+++ demo/index.js
@@ -2,17 +2,19 @@ if (typeof WebAssembly == "undefined") {
   alert('Your browser doesn\'t support WebAssembly')
 }
 
-var importObject = { imports: { } };
+var importObject = {
+  mem : new WebAssembly.Memory({ init : 10, max : 100 })
+}
 
 fetch('./test.wasm').then(response => response.arrayBuffer())
 .then(bytes => instantiate(bytes, importObject))
 .then(instance => {
-  console.log(instance.exports.test());
+  console.log(instance.exports.test())
 })
 
 function instantiate(bytes, imports) {
-  let compiled = WebAssembly.compile(bytes);
+  let compiled = WebAssembly.compile(bytes)
 
-  console.log('compiled: ', compiled)
-  return compiled.then(m => new WebAssembly.Instance(m, imports));
+  console.log('compiled')
+  return compiled.then(m => new WebAssembly.Instance(m, imports))
 }
